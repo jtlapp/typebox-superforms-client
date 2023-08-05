8f6ebb9068096c78e73e97d2bb021f8f24887c86
Joseph T. Lapp <arachnojoe@gmail.com>
2023-08-05 12:36:28 -0400

    test: basic test of client-side superValidateSync

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<p><a href="/typebox">TypeBox Forms</a></p>
+<h1>Test Forms</h1>
+<ul>
+  <li><a href="/typebox-val-sync">TypeBox: superValidateSync</a></li>
+</ul>
 
-<p><a href="/zod">Zod Forms</a></p>
+<p><a href="/zod">Zod Forms</a> (for reverse engineering)</p>
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,45 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+test.describe("superValidateSync", () => {
+  test("client-side", async ({ page }) => {
+    await page.goto("/typebox-val-sync");
+    await page.click("#client-side button");
+    await expect(page.locator("body")).toContainText("updated");
+
+    expect(await page.inputValue("#client-side input[name=name]")).toEqual(
+      "Jane"
+    );
+    await expect(page.locator("#client-side .name_errors")).toHaveCount(0);
+
+    expect(await page.inputValue("#client-side input[name=nickname]")).toEqual(
+      ""
+    );
+    await expect(page.locator("#client-side .nickname_errors")).toHaveCount(0);
+
+    expect(await page.inputValue("#client-side input[name=age]")).toEqual("");
+    await expect(page.locator("#client-side .age_errors")).toContainText(
+      "Must be a number >= 13"
+    );
+
+    expect(await page.inputValue("#client-side input[name=siblings]")).toEqual(
+      ""
+    );
+    await expect(page.locator("#client-side .siblings_errors")).toHaveCount(0);
+
+    expect(await page.inputValue("#client-side input[name=email]")).toEqual("");
+    await expect(page.locator("#client-side .email_errors")).toContainText(
+      "string"
+    );
+    await expect(page.locator("#client-side .email_errors")).toContainText(
+      "10"
+    );
+    await expect(page.locator("#client-side .email_errors")).toContainText(
+      "pattern"
+    );
+
+    expect(await page.isChecked("#client-side input[name=agree]")).toEqual(
+      false
+    );
+    await expect(page.locator("#client-side .agree_errors")).toHaveCount(0);
+  });
 });
