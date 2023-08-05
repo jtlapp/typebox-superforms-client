6994bf1c176dba83805a4633e4786c71a679609b
Joseph T. Lapp <arachnojoe@gmail.com>
2023-08-05 13:36:07 -0400

    test: organized to support more tests

--- tests/test.ts
+++ tests/test.ts
@@ -3,53 +3,55 @@ import { test } from "@playwright/test";
 import { waitForUpdate, checkForm, type ExpectedForm } from "./util.js";
 
 test.describe("superValidateSync", () => {
-  test("client-side", async ({ page }) => {
-    await page.goto("/typebox-val-sync");
+  test.describe("client-side", () => {
+    test("initial form behavior", async ({ page }) => {
+      await page.goto("/typebox-val-sync");
 
-    // Verify initial form.
+      // Verify initial form.
 
-    const initialForm: ExpectedForm = {
-      name: {
-        value: "Jane",
-        errors: [],
-      },
-      nickname: {
-        value: "",
-        errors: [],
-      },
-      age: {
-        value: "",
-        errors: [],
-      },
-      siblings: {
-        value: "",
-        errors: [],
-      },
-      email: {
-        value: "",
-        errors: [],
-      },
-      agree: {
-        value: false,
-        errors: [],
-      },
-    };
-    await checkForm(page, "#client-side", initialForm);
+      const initialForm: ExpectedForm = {
+        name: {
+          value: "Jane",
+          errors: [],
+        },
+        nickname: {
+          value: "",
+          errors: [],
+        },
+        age: {
+          value: "",
+          errors: [],
+        },
+        siblings: {
+          value: "",
+          errors: [],
+        },
+        email: {
+          value: "",
+          errors: [],
+        },
+        agree: {
+          value: false,
+          errors: [],
+        },
+      };
+      await checkForm(page, "#client-side", initialForm);
 
-    // Verify submitting initial form.
+      // Verify submitting initial form.
 
-    await page.click("#client-side button");
-    await waitForUpdate(page);
-    await checkForm(page, "#client-side", {
-      ...initialForm,
-      age: {
-        value: "",
-        errors: ["Must be a number >= 13"],
-      },
-      email: {
-        value: "",
-        errors: ["string", "10", "pattern"],
-      },
+      await page.click("#client-side button");
+      await waitForUpdate(page);
+      await checkForm(page, "#client-side", {
+        ...initialForm,
+        age: {
+          value: "",
+          errors: ["Must be a number >= 13"],
+        },
+        email: {
+          value: "",
+          errors: ["string", "10", "pattern"],
+        },
+      });
     });
   });
 });
